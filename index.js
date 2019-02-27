524ca6dc53309c43064951dcb79c6e2e08c6fc21
Sebastiaan Deckers <sebdeckers83@gmail.com>
2019-02-27 22:24:28 +0800

    Adding an option to set bits_per_item

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 'use strict'
 
 const binding = require('node-gyp-build')(__dirname)
-module.exports = binding.CuckooFilter
+
+function CuckooFilter (totalSize, bitsPerItem = 12) {
+  switch (bitsPerItem) {
+    case 2:
+    case 4:
+    case 8:
+    case 12:
+    case 16:
+    case 32:
+      return new binding['CuckooFilter' + bitsPerItem](totalSize)
+    default:
+      throw new Error('The second argument must be: 2, 4, 8, 12, 16, or 32.')
+  }
+}
+
+module.exports = CuckooFilter
